2867a4c4fdcc9919d0a7785e1e71beff6470a0ac
contact.halaspec@gmail.com <91801806+Halaspec@users.noreply.github.com>
2023-01-18 21:32:51 +0100

    Update questions.js

--- questions.js
+++ questions.js
@@ -82,5 +82,117 @@ options: [
 "VRAI",
 "FAUX"
 ]
-}
+},
+    {
+numb: 9,
+question: "La main invisible est un mécanisme :",
+answer: "Autorégulateur du marché",
+options: [
+"Préconisé par le courant keynésien",
+"Autorégulateur du marché",
+"Qui préconise l’intervention de l’Etat"
+]
+},
+{
+numb: 10,
+question: "D’après les indices de Gini, quel est le pays le plus égalitaire en 2015 :",
+answer: "Island (G = 0,246)",
+options: [
+"Canada (G = 0,313)",
+"France (G = 0,297)",
+"Slovénie (G = 0,251)",
+"Island (G = 0,246)"
+]
+},
+{
+numb: 11,
+question: "La formation brute des capitaux fixes peut être considérée comme de l’investissement public :",
+answer: "VRAI",
+options: [
+"VRAI",
+"FAUX"
+]
+},
+{
+numb: 12,
+question: "Quand la demande de travail est supérieure à l’offre de travail, la concurrence se fait entre :",
+answer: "Les entreprises",
+options: [
+"Les ménages",
+"Les entreprises"
+]
+},
+{
+numb: 13,
+question: "Les activités de déforestation fait baisser le PIB :",
+answer: "VRAI",
+options: [
+"VRAI",
+"FAUX"
+]
+},
+{
+numb: 14,
+question: "Le capitalisme contemporain peut être considéré comme le capitalisme actionnarial :",
+answer: "VRAI",
+options: [
+"VRAI",
+"FAUX"
+]
+},
+    {
+numb: 15,
+question: "Dans le circuit économique, le flux réel est la contrepartie du flux monétaire ?",
+answer: "VRAI",
+options: [
+"VRAI",
+"FAUX"
+]
+},
+{
+numb: 16,
+question: "Un PIB est forcément un reflet du bien-être de la population ?",
+answer: "FAUX",
+options: [
+"VRAI",
+"FAUX"
+]
+},
+{
+numb: 17,
+question: "Dans le modèle néoclassique, le pouvoir de négociation élevé syndical crée du chômage volontaire ?",
+answer: "FAUX",
+options: [
+"VRAI",
+"FAUX"
+]
+},
+{
+numb: 18,
+question: "La formation brute des capitaux fixes peut être considérée comme de l’investissement public ?",
+answer: "VRAI",
+options: [
+"VRAI",
+"FAUX"
+]
+},
+{
+numb: 19,
+question: "Médecin sans frontière est une organisation productive :",
+answer: "Non-marchande",
+options: [
+"Marchande",
+"Non-marchande"
+]
+},
+{
+numb: 20,
+question: "En matière de création monétaire, les crédits font les dépôts ?",
+answer: "VRAI",
+options: [
+"VRAI",
+"FAUX"
+]
+},
+    
 ];
